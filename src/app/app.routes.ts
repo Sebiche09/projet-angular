c99d1205775a8977ff259b21fb8e11bcf4d6c5bd
Seb <sebastien.vleminckx@outlook.fr>
2024-09-11 22:17:00 +0200

    Votre message de commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,11 @@ import { DashboardComponent } from '../components/dashboard/dashboard.component'
 import { AddTaskComponent } from '../components/add-task/add-task.component';
 import { TaskDetailComponent } from '../components/task-detail/task-detail.component';
 export const routes: Routes = [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: '', redirectTo: 'task', pathMatch: 'full' },
     {path: 'dashboard', component: DashboardComponent},
     { path: 'exemple', component: ExempleComponent },
     { path: 'message', component: MessageComponent },
     { path: 'task', component: TaskComponent },
     { path: 'addTask', component: AddTaskComponent },
     { path: 'taskDetail/:id', component: TaskDetailComponent },
-     // Redirection par défaut vers exemple
 ];
